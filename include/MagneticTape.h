edde9fac23912f20ee12ed510084eb42e88b2620
v3nflaxine <dmitriyart8@gmail.com>
2026-03-24 22:30:52 +0300

    Update MagneticTape.h

--- include/MagneticTape.h
+++ include/MagneticTape.h
@@ -1,61 +1,47 @@
-
 #pragma once
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
-#include <algorithm>
 #include "Constants.h" 
-
-using namespace std;
 
 class MagneticTape {
 private:
-    // Класс узла двусвязного списка
-    class TapeNode {
-    public:
-        string filename;
+    struct TapeNode {
+        std::string filename;
         int sizeKb;
-        string createTime;
-        TapeNode* next;
-        TapeNode* prev;
+        std::string createTime;
+        TapeNode* next = nullptr;
+        TapeNode* prev = nullptr;
 
-        TapeNode(string name, int size, string time)
-            : filename(name), sizeKb(size), createTime(time), next(nullptr), prev(nullptr) {}
+        TapeNode(std::string name, int size, std::string time)
+            : filename(name), sizeKb(size), createTime(time) {}
     };
 
     TapeNode* head;
     TapeNode* tail;
     int count;
-    int strToInt(string data);
-    bool isValidInput(const string& input, bool isNumber = false);
-    vector<string> splitString(const string& str, char delimiter);
+
+    // Внутренние утилиты для работы со списком
+    std::vector<std::string> splitString(const std::string& str, char delimiter);
     TapeNode* getNodeAtIndex(int index);
 
 public:
-    MagneticTape();     // Конструктор
-    ~MagneticTape();    // Деструктор
+    MagneticTape();
+    ~MagneticTape();
 
     void clearAll();
     bool isEmpty() const;
     int getCount() const;
 
-    // Основные операции
-    void addToTail(string name, int size, string time);
-    bool insertAt(int index, string name, int size, string time);
+    void addToTail(std::string name, int size, std::string time);
+    bool insertAt(int index, std::string name, int size, std::string time);
     bool deleteAt(int index);
     void display() const;
 
-    // Работа с файлами
-    bool exportToFile(const string& filename);
-    bool importFromFile(const string& filename);
+    bool exportToFile(const std::string& filename);
+    bool importFromFile(const std::string& filename);
 
-    // Поиск и сортировка
-    void search(int searchType, string value);
+    void search(int searchType, const std::string& value);
     void sort(int fieldType);
-
-    // Публичные прокси-методы
-    bool publicIsValidInput(const string& input, bool isNumber = false);
-    int publicStrToInt(const string& data);
 };
-
